6cc34af8aa5f04ec3662c40cd92a235967d00721
Dannysesi <daniel.olusesi20@gmail.com>
2023-02-27 00:39:15 +0100

    basic calculator using python(tried power by 2)

--- calculator.py
+++ calculator.py
@@ -6,16 +6,19 @@ def multiply(a, b):
     return a*b
 def divide(a, b):
     return a/b
+def x2(a):
+    return a ** 2
 
 print('Select Operation')
 print('1. Add')
 print('2. Subtract')
 print('3. Multiply')
 print('4. Divide')
+print('5. Power 2')
 
 while True:
-    x = input("enter 1 to add, 2 to subtract, 3 to multiply, 4 to divide: ")
-    if x in ('1', '2', '3', '4'):
+    x = input("enter 1 to add, 2 to subtract, 3 to multiply, 4 to divide, 5 to power by 2: ")
+    if x in ('1', '2', '3', '4', '5'):
         w = float(input("enter value: "))
         v = float(input("enter value: "))
         if x == '1':
@@ -26,6 +29,8 @@ while True:
             print(w, '*', v, '=', multiply(w, v))
         elif x == '4':
             print(w, '/', v, '=', divide(w, v))
+        if x == '5':
+            print(w, x2(w))
         newcal = input("new calculation (yes/no): ")
         if newcal == "no":
             break
