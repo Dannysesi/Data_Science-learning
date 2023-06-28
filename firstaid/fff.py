70cf8260f5fdc43ef12273627dd01698c1bc93cb
Dannysesi <daniel.olusesi20@gmail.com>
2023-06-28 23:30:52 +0100

    removed typewriter

--- firstaid/fff.py
+++ firstaid/fff.py
@@ -99,23 +99,7 @@ from requests import Response
 
 # st.set_page_config(layout="wide")
 
-import time
 
-# def typingPrint(text):
-# 	for characters in text:
-# 		st.write(characters)
-# 		# sys.stdout.flush()
-# 		time.sleep(0.05)
-
-def typewriter_effect(text):
-    # Create an empty slot to display the text
-    # text_slot = st.empty()
-    # Iterate over each character in the text
-    for char in text:
-        # Add the current character to the text_slot
-        st.write(char, end='', flush=True)
-        # Pause for a short duration to create the typewriter effect
-        time.sleep(0.05)
 
 unique_key = 0
 def chat():
@@ -135,7 +119,7 @@ def chat():
 			if tg['tag'] == tag:
 				responses = tg['responses']
 				st.write('Chabot🤖: ')
-				typewriter_effect(responses)
+				st.markdown(responses)
 				print("\n")
 				break
 	else:
