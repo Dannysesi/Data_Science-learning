12464fd3fdbb011895bfd7e929380c28055669ef
Dannysesi <daniel.olusesi20@gmail.com>
2023-08-07 15:54:29 +0100

    text summarizer

---
+++ TextSum/textsum.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import time
+import random
+
+@st.cache_resource
+def load_model():
+    model_name = "google/pegasus-xsum"
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name)
+    return model, tokenizer
+
+def summarize_text(model, tokenizer, text, max_length=150, min_length=50):
+    inputs = tokenizer(text, return_tensors="pt", max_length=max_length, min_length=min_length, truncation=True)
+    summary_ids = model.generate(inputs.input_ids, num_beams=4, max_length=150, min_length=50, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary
+
+def main():
+    st.title("Text Summarizer with PEGASUS")
+
+    model, tokenizer = load_model()  # Load the model only once
+
+    # Input text
+    input_text = st.text_area("Enter your text here:", height=300)
+
+    if st.button("Summarize"):
+        progress_text = "Operation in progress. Please wait."
+        my_bar = st.progress(0, text=progress_text)
+
+        duration = random.uniform(27, 33)
+
+        # Number of steps in the progress bar
+        steps = 100
+
+        # Time interval between steps
+        time_interval = duration / steps
+
+        for percent_complete in range(steps):
+            time.sleep(time_interval)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+        if input_text:
+            summary = summarize_text(model, tokenizer, input_text)  # Use the loaded model
+            st.subheader("Summary:")
+            st.write(summary)
+        else:
+            st.warning("Please enter some text to summarize.")
+
+if __name__ == "__main__":
+    main()
